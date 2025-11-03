8d0d701172f4c01c65149e16a03c0d9dd7dcb033
Dmitry.Mazitov <dmitry.mazitov@x5.ru>
2025-11-03 15:52:32 +0300

    try

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type build } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/experiments/',
+  base: mode === 'development' ? '' : '/experiments',
+
   build: {
     outDir: 'build',
   },
-});
+}));
