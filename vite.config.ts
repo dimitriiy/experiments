93e7e17ef41733fcb754e15f5c1de1d45e73bc0d
Dmitry.Mazitov <dmitry.mazitov@x5.ru>
2025-11-03 15:42:46 +0300

    try

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/experiments',
+  base: '/experiments/',
   build: {
     outDir: 'build',
   },
